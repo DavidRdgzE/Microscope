d7530c532b52eaec9280222f1bb7b94fd59adebb
danelly-dmh <danelly.dmh@gmail.com>
2017-05-31 08:19:26 -0600

    Mejoras en la interfaz de usuario

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,16 +1,21 @@
 Template.postItem.helpers({
-ownPost: function() {    
-	return this.userId === Meteor.userId(); 
-	 },
-
-  domain: function() {
-    var a = document.createElement('a');
-    a.href = this.url;
-    return a.hostname;
-  },
-  commentsCount: function() {
-    return Comments.find({postId: this._id}).count();
-}
+	ownPost: function() {    
+		return this.userId === Meteor.userId();
+	},
+	domain: function() {
+    	var a = document.createElement('a');
+    	a.href = this.url;
+    	return a.hostname;
+	},
+	upvotedClass: function() {
+  		var userId = Meteor.userId();
+  		if (userId && !_.include(this.upvoters, userId)) {
+  			return 'btn-primary upvotable';
+  		}
+  		else {
+  			return 'disabled';
+  		}
+	}
 });
 Template.postItem.events({
 	e.preventDefault();
