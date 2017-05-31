83f3ced97043300c261674e4f304f52a1cbe6f2f
karelyoo <karelyyy_ooolivas@hotmail.com>
2017-05-30 18:43:27 -0600

    Se crearon mecanismos simples de publicacion y suscripcion

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,8 @@
-Meteor.publish('allPosts', function(){
-  return Posts.find({'author':'Tom'}, {fields: {
-    date: false
-  }});
+Meteor.publish('posts', function() {
+  return Posts.find();
 });
 
-Meteor.publish('comments', function() {
-  return Comments.find();
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
 });
